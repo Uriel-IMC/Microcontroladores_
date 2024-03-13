91b7c8024cd5a66fd6d2b2380475ca87bb301911
Uriel-IMC <159559420+Uriel-IMC@users.noreply.github.com>
2024-03-13 00:17:50 -0600

    Carpetas
    creé las carpetas necesarias para act. 2

---
+++ Actividad 2/Actividad 2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Actividad 2.c
+ *
+ * Created: 13/03/2024 12:08:16 a. m.
+ * Author : Uriel I MCarrillo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
